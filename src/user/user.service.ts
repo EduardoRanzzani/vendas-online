689f6bf1a1e0d90d5649de57db35b3982dd359dc
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2024-01-10 19:51:15 -0400

    feat: add message if user already exists

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { hash } from 'bcrypt';
 import { PrismaService } from '../prisma/prisma.service';
@@ -9,6 +13,14 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+    const user = await this.findUserByEmail(createUserDTO.email).catch(
+      () => undefined,
+    );
+
+    if (user) {
+      throw new ConflictException('User already exists');
+    }
+
     const saltOrRounds = 10;
     const passwordHashed = await hash(createUserDTO.password, saltOrRounds);
 
